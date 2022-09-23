54157e015b28a15f329b06addcff0dce52176fad
shyamtiwari02062001 <shyam02062001@gmail.com>
2022-09-23 16:06:42 +0530

    created event and tested via postman:

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const port = 8080;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("new_visitor", (user) => {
+    socket.user = user;
+    console.log("new Visitor", user);
   });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on :${port}`);
 });
